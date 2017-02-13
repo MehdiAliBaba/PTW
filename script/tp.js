5cefb866eedf91e8ba051d9e684a0fde70db8e23
Jules <aubertjules@gmail.com>
2017-02-13 23:06:46 +0100

    Begin real project
    It's time to end playing with tech, let's create !

--- script/tp.js
+++ script/tp.js
@@ -1,4 +1,35 @@
-AFRAME.registerComponent('tp1', {
+// Magasin 1 ; Stand 1
+AFRAME.registerComponent('mag1_stand1', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first-camera');
+        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+    });
+  }
+});
+
+// Magasin 1 ; Stand 2
+AFRAME.registerComponent('mag1_stand2', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first-camera');
+        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+    });
+  }
+});
+
+// Magasin 2 ; Stand 1
+AFRAME.registerComponent('mag2_stand1', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first-camera');
+        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+    });
+  }
+});
+
+// Magasin 2 ; Stand 2
+AFRAME.registerComponent('mag2_stand2', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
       var secondCameraEl = document.querySelector('#first-camera');
