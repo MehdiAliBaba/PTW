4c50836af99482e5fa6884ffa5f049ae4d25a422
Jules <aubertjules@gmail.com>
2017-02-13 22:22:03 +0100

    Delete unnecessary line in tp.js

--- script/tp.js
+++ script/tp.js
@@ -2,7 +2,6 @@ AFRAME.registerComponent('tp1', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
       var secondCameraEl = document.querySelector('#first-camera');
-        secondCameraEl.setAttribute('camera', 'active', true);
         secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
     });
   }
