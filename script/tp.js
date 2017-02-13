7fdd5c11cedf3ae459d92a415ffded0a63ed0487
Jules <aubertjules@gmail.com>
2017-02-13 21:03:51 +0100

    We can TP camera anywhere, camera's cursor remains fine. Hurray !

---
+++ script/tp.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('tp1', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first-camera');
+        secondCameraEl.setAttribute('camera', 'active', true);
+        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+    });
+  }
+});
