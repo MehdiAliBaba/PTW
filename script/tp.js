2d17f30e16842b8725dbc98e6d5d16d6595cb912
Jules <aubertjules@gmail.com>
2017-02-14 00:06:15 +0100

    Hub is working

--- script/tp.js
+++ script/tp.js
@@ -1,3 +1,21 @@
+// Hub ; Porte bleue
+AFRAME.registerComponent('blue_door', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      window.location.href = "mag1.html";
+    });
+  }
+});
+
+// Hub ; Porte verte
+AFRAME.registerComponent('green_door', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      window.location.href = "mag2.html";
+    });
+  }
+});
+
 // Magasin 1 ; Stand 1
 AFRAME.registerComponent('mag1_stand1', {
   init: function () {
