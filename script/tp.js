c74ec14d29388d075b456ce247eec06059d307d0
Jules <aubertjules@gmail.com>
2017-02-16 19:29:56 +0100

    Hub is ok; Shops need have the scene
    Shops need a door to come back to the hub, 3d products with prices and
    total price printed somewhere

--- script/tp.js
+++ script/tp.js
@@ -16,12 +16,21 @@ AFRAME.registerComponent('green_door', {
   }
 });
 
+// Magasins ; Porte rouge
+AFRAME.registerComponent('red_door', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      window.location.href = "index.html";
+    });
+  }
+});
+
 // Magasin 1 ; Stand 1
 AFRAME.registerComponent('mag1_stand1', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-      var secondCameraEl = document.querySelector('#first-camera');
-        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+      var secondCameraEl = document.querySelector('#first_camera');
+      secondCameraEl.setAttribute('position', { x: 0, y: 0.6, z: 0 });
     });
   }
 });
@@ -30,8 +39,18 @@ AFRAME.registerComponent('mag1_stand1', {
 AFRAME.registerComponent('mag1_stand2', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-      var secondCameraEl = document.querySelector('#first-camera');
-        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+      var secondCameraEl = document.querySelector('#first_camera');
+        secondCameraEl.setAttribute('position', { x: -9, y: 1, z: -4 });
+    });
+  }
+});
+
+// Magasin 1 ; Stand 3
+AFRAME.registerComponent('mag1_stand3', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first_camera');
+        secondCameraEl.setAttribute('position', { x: 8, y: 1, z: -6 });
     });
   }
 });
@@ -40,8 +59,8 @@ AFRAME.registerComponent('mag1_stand2', {
 AFRAME.registerComponent('mag2_stand1', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-      var secondCameraEl = document.querySelector('#first-camera');
-        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+      var secondCameraEl = document.querySelector('#first_camera');
+        secondCameraEl.setAttribute('position', { x: 0, y: -1, z: 0 });
     });
   }
 });
@@ -50,8 +69,18 @@ AFRAME.registerComponent('mag2_stand1', {
 AFRAME.registerComponent('mag2_stand2', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-      var secondCameraEl = document.querySelector('#first-camera');
-        secondCameraEl.setAttribute('position', { x: 0, y: 5, z: 0 });
+      var secondCameraEl = document.querySelector('#first_camera');
+        secondCameraEl.setAttribute('position', { x: -9.5, y: 1, z: -4 });
+    });
+  }
+});
+
+// Magasin 2 ; Stand 3
+AFRAME.registerComponent('mag2_stand3', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      var secondCameraEl = document.querySelector('#first_camera');
+        secondCameraEl.setAttribute('position', { x: -9.5, y: 1, z: -4 });
     });
   }
 });
