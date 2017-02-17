cd269c4139034c17ffbb914cfe6ae00f624b02b5
Jules <aubertjules@gmail.com>
2017-02-17 01:05:00 +0100

    Doors to cgo to the Hub : Ok; Code to add price : WIP; Need to add 3d models with prices

--- script/price.js
+++ script/price.js
@@ -1,1 +1,13 @@
+var bought = 0;
 var total = 0;
+AFRAME.registerComponent('total_price', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+      bought = 0;
+      total += bought;
+      var totalPrice = document.querySelector('#to_pay');
+      totalPrice.setAttribute('value', total);
+      bought = 0;
+    });
+  }
+});
